26508dc2a67eab345be1ce908b81220ef45df557
Chris Seaton <chris@chrisseaton.com>
2022-10-23 00:59:55 +0100

    Basic typeset 1797-1812

---
+++ notes/cheshire-yeomanry/nomrolls/md2tex.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+people_groups = [[nil, []]]
+
+ARGF.each_line do |line|
+  case line
+  when /^##\s+(.*)$/
+    people_groups.pop if people_groups.last.last.empty?
+    people_groups.push [$1, []]
+  when /^\* (.*:\s+)?(.*)$/
+    appointment = $1
+    name = $2
+    name.gsub! /(\d)+st/, '\1\nth'
+    appointment.sub! /:\s+$/, '' if appointment
+    people_groups.last.last.push [appointment, name]
+  end
+end
+
+people_groups.each do |title, people|
+  if title
+    puts '\begin{center}'
+    puts '  \Large'
+    puts "  \\textbf{#{title}}"
+    puts '\end{center}'
+    puts
+  end
+
+  if people.all? { |appointment, name| appointment.nil? }
+    puts '\begin{multicols}{2}'
+    puts '  \noindent'
+    people.each do |_, name|
+      puts "  #{name} \\\\"
+    end
+    puts '\end{multicols}'
+    puts
+  else
+    puts '\begin{center}'
+    puts '  \begin{tabular}{rl}'
+    people.each do |appointment, name|
+      if appointment
+        puts "    #{appointment} & #{name} \\\\"
+      else
+        puts "    & #{name} \\\\"
+      end
+    end
+    puts '  \end{tabular}'
+    puts '\end{center}'
+    puts
+  end
+end
