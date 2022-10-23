2d6ed4274aeb414cff220c1ee5c623ce1ee7714c
Chris Seaton <chris@chrisseaton.com>
2022-10-23 21:23:04 +0100

    Basic typsetting of officers

--- notes/cheshire-yeomanry/nomrolls/md2tex.rb
+++ notes/cheshire-yeomanry/nomrolls/md2tex.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 
+officers = ARGV.delete('--officers')
+
 people_groups = [[nil, []]]
 
 ARGF.each_line do |line|
   case line
   when "\n"
-    people_groups.pop if people_groups.last.last.empty?
-    people_groups.push [nil, []]
+    if people_groups.last.first.nil? && people_groups.last.last.empty?
+      people_groups.pop
+      people_groups.push [nil, []]
+    end
   when /^##\s+(.*)$/
-    people_groups.pop if people_groups.last.last.empty?
+    people_groups.pop if people_groups.last.first.nil? && people_groups.last.last.empty?
     people_groups.push [$1, []]
   when /^\* (.*:\s+)?(.*)$/
     appointment = $1
@@ -22,35 +26,50 @@ ARGF.each_line do |line|
   end
 end
 
-people_groups.each do |title, people|
-  if title
-    puts '\begin{center}'
-    puts '  \Large'
-    puts "  \\textbf{#{title}}"
-    puts '\end{center}'
-    puts
-  end
-
-  if people.all? { |appointment, name| appointment.nil? }
-    puts '\begin{multicols}{2}'
-    puts '  \noindent'
+if officers
+  puts '\begin{multicols}{2}'
+  puts '  \noindent'
+  
+  people_groups.each do |year, people|
+    puts "  \\section*{#{year}}"
     people.each do |_, name|
       puts "  #{name} \\\\"
     end
-    puts '\end{multicols}'
-    puts
-  else
-    puts '\begin{center}'
-    puts '  \begin{tabular}{rl}'
-    people.each do |appointment, name|
-      if appointment
-        puts "    #{appointment} & #{name} \\\\"
-      else
-        puts "    & #{name} \\\\"
-      end
+  end
+  
+  puts '\end{multicols}'
+  puts
+else
+  people_groups.each do |title, people|
+    if title
+      puts '\begin{center}'
+      puts '  \Large'
+      puts "  \\textbf{#{title}}"
+      puts '\end{center}'
+      puts
     end
-    puts '  \end{tabular}'
-    puts '\end{center}'
-    puts
+
+    if people.all? { |appointment, name| appointment.nil? }
+      puts '\begin{multicols}{2}'
+      puts '  \noindent'
+      people.each do |_, name|
+        puts "  #{name} \\\\"
+      end
+      puts '\end{multicols}'
+      puts
+    else
+      puts '\begin{center}'
+      puts '  \begin{tabular}{rl}'
+      people.each do |appointment, name|
+        if appointment
+          puts "    #{appointment} & #{name} \\\\"
+        else
+          puts "    & #{name} \\\\"
+        end
+      end
+      puts '  \end{tabular}'
+      puts '\end{center}'
+      puts
+    end
   end
 end
